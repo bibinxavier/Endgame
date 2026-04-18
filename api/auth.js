19cd770ff2cdea08c121a11bd3278225fc78ef09
bibinxavier <bibinxavier54@gmail.com>
2026-04-18 20:28:11 +0530

    Create authentication API endpoints

---
+++ api/auth.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/UserController');
+
+// Register user
+router.post('/register', UserController.register);
+
+// Login user
+router.post('/login', UserController.login);
+
+// Logout user
+router.post('/logout', UserController.logout);
+
+// Get all modules
+router.get('/modules', UserController.getModules);
+
+module.exports = router;
